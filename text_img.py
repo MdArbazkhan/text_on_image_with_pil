17e939664eae38bd6297bc9433fb25f7f865ae7b
Arbaz Khan <arbazkhan2772@gmail.com>
2021-12-15 20:20:59 +0530

    initial commit

---
+++ text_img.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageFont, ImageDraw
+
+
+def text_image(image_path,title_text):
+    try:
+        my_image = Image.open(image_path)
+    except:
+        return None
+    width, height = my_image.size
+    # arial.ttf - I have downloaded this font from other source
+    # source url - https://github.com/JotJunior/PHP-Boleto-ZF2/blob/master/public/assets/fonts/arial.ttf
+    title_font = ImageFont.truetype('arial.ttf', int(float(width) / 8))
+    image_editable = ImageDraw.Draw(my_image)
+    image_editable.text((15,15), title_text, (237, 230, 211), font=title_font)
+
+    # create path to save the text image
+    img_path_split = image_path.split('/')
+    if len(img_path_split)>1:
+        img_path_split[-1] = 'text_' + img_path_split[-1]
+        text_img_file = '/'.join(img_path_split)
+    elif len(img_path_split)==1:
+        text_img_file = 'text_' + image_path
+
+    my_image.save(text_img_file)
+
+    return text_img_file
+
+
+print(text_image('/home/gadget/Desktop/text_image/jubliee.jpeg','Jubilee Park'))
